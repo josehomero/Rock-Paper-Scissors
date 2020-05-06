bd46841a8277ec64dbcefa0aa368db58b2160512
josehomero <jose.homero.rios@outlook.com>
2020-05-05 21:08:06 -0700

    picture buttons created

---
+++ script.js
@@ -0,0 +1,8 @@
+const textDiv = document.getElementById('text-div');
+
+const pictureButtons = document.querySelectorAll('.picture-buttons');
+pictureButtons.forEach(button => {
+    button.addEventListener('click', e => {
+        
+    })
+})
