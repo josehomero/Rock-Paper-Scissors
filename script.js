5958f48989fb740d591acda87c1d34ff194486c5
josehomero <jose.homero.rios@outlook.com>
2020-05-06 21:23:27 -0700

    random opponent result

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-const textDiv = document.getElementById('text-div');
 const opponentResult = ['Rock', 'Paper', 'Scissors'];
 
-//const createTextInDiv = function (text) {
-    //var divText = document.createElement('p');
-    //textDiv.textContent = opponentResult[Math.floor(Math.random() * 3)];
-  //  textDiv.appendChild(divText);
-//}
-
+const textDiv = document.getElementById('text-div');
 const pictureButtons = document.querySelectorAll('.picture-buttons');
 pictureButtons.forEach(button => {
     button.addEventListener('click', e => {
-        function displayedResult() {
-            textDiv.textContent = opponentResult[Math.floor(Math.random() * 3)]
-        }
+        textDiv.textContent = opponentResult[Math.floor(Math.random() * 3)];
     })
 })
