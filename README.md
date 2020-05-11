4a498521825277b3989e5bfed097dcb11fc66f63
josehomero <jose.homero.rios@outlook.com>
2020-05-11 15:42:16 -0700

    Finished

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ Ready to play?!
 
 Check it out https://josehomero.github.io/Rock-Paper-Scissors/
 
-<img width="516" alt="rps" src="https://user-images.githubusercontent.com/58618050/81463267-22d3f380-916d-11ea-8cc7-6063a709087a.PNG">
+<img width="569" alt="Updated rps pic" src="https://user-images.githubusercontent.com/58618050/81609532-ed0d5580-938c-11ea-883e-7d1c6d184e1a.PNG">
 
 ## Summary
 
-This project solitified things I have learned with JacaScript, specifically targeting elements within an if statement. 
+This project solitified things I have learned with JacaScript, specifically targeting elements within an if statement. This project also gave me a taste of looking into CSS and styling.
 
 It didn't take as long as I thought it would but it showed me how much I've started to retain in my journey as a Software-Developer.
 
--- script.js
+++ script.js
@@ -1,57 +1,52 @@
-const opponentResult = ['Rock', 'Paper', 'Scissors'];
+const opponentResult = ['Computer chooses Rock', 'Computer chooses Paper', 'Computer chooses Scissors'];
 let userWins = 0;
 let computerWins = 0;
 
+let yourScore = document.getElementById('your-score');
 function userWinsTheGame() {
-    userWins +=1
-    yourScore.value = userWins;
-    yourScore.textContent = yourScore.value;
+    userWins += 1
+    yourScore.textContent = userWins;
 }
 
+let computerScore = document.getElementById('computer-score');
 function computerWinsTheGame() {
     computerWins += 1;
-    computerScore.value = computerWins;
+    computerScore.textContent = computerWins;
 }
 
 const textDiv = document.getElementById('text-div');
-let yourScore = document.getElementsByTagName('your-score');
-let computerScore = document.getElementById('computer-score');
+
 const pictureButtons = document.querySelectorAll('.picture-buttons');
 pictureButtons.forEach(button => {
     button.addEventListener('click', e => {
         textDiv.textContent = opponentResult[Math.floor(Math.random() * 3)];
 
-
-
-        if(e.target.id === 'rock' && textDiv.textContent === 'Rock') {
+        if (e.target.id === 'rock' && textDiv.textContent === 'Computer chooses Rock') {
             return textDiv.textContent += ": It's a DRAW!";
-        } else if (e.target.id === 'rock' && textDiv.textContent === 'Scissors') {
-            //add 1 to userWins
+        } else if (e.target.id === 'rock' && textDiv.textContent === 'Computer chooses Scissors') {
             userWinsTheGame()
-            console.log(yourScore.value);
-            //debugger;
-            //update your-score <p> to = userWins variable
             return textDiv.textContent += ": YOU WIN!";
-        } else if (e.target.id === 'rock' && textDiv.textContent === 'Paper') {
+        } else if (e.target.id === 'rock' && textDiv.textContent === 'Computer chooses Paper') {
             computerWinsTheGame();
             return textDiv.textContent += ": You lose!";
         }
 
-        if(e.target.id === 'scissors' && textDiv.textContent === 'Rock') {
+        if (e.target.id === 'scissors' && textDiv.textContent === 'Computer chooses Rock') {
             computerWinsTheGame();
             return textDiv.textContent += " : You lose!";
-        } else if (e.target.id === 'scissors' && textDiv.textContent === 'Scissors') {
+        } else if (e.target.id === 'scissors' && textDiv.textContent === 'Computer chooses Scissors') {
             return textDiv.textContent += ": It's a DRAW!";
-        } else if (e.target.id === 'scissors' && textDiv.textContent === 'Paper') {
+        } else if (e.target.id === 'scissors' && textDiv.textContent === 'Computer chooses Paper') {
+            userWinsTheGame();
             return textDiv.textContent += ": YOU WIN!";
         }
 
-        if(e.target.id === 'paper' && textDiv.textContent === 'Rock') {
+        if (e.target.id === 'paper' && textDiv.textContent === 'Computer chooses Rock') {
             userWinsTheGame()
             return textDiv.textContent += ": YOU WIN!";
-        } else if (e.target.id === 'paper' && textDiv.textContent === 'Paper') {
+        } else if (e.target.id === 'paper' && textDiv.textContent === 'Computer chooses Paper') {
             return textDiv.textContent += ": It's a DRAW!";
-        } else if (e.target.id === 'paper' && textDiv.textContent === 'Scissors') {
+        } else if (e.target.id === 'paper' && textDiv.textContent === 'Computer chooses Scissors') {
             computerWinsTheGame();
             return textDiv.textContent += " : You lose!";
         }
