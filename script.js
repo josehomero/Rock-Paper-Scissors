088073a3882e6271e4dac03b0a87340204f18433
josehomero <jose.homero.rios@outlook.com>
2020-05-11 13:48:48 -0700

    updated

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ const opponentResult = ['Rock', 'Paper', 'Scissors'];
 let userWins = 0;
 let computerWins = 0;
 
+function userWinsTheGame() {
+    userWins +=1
+    yourScore.value = userWins;
+    yourScore.textContent = yourScore.value;
+}
+
+function computerWinsTheGame() {
+    computerWins += 1;
+    computerScore.value = computerWins;
+}
+
 const textDiv = document.getElementById('text-div');
 let yourScore = document.getElementsByTagName('your-score');
-let computerscore = document.getElementById('computer-score');
+let computerScore = document.getElementById('computer-score');
 const pictureButtons = document.querySelectorAll('.picture-buttons');
 pictureButtons.forEach(button => {
     button.addEventListener('click', e => {
@@ -14,28 +25,34 @@ pictureButtons.forEach(button => {
 
         if(e.target.id === 'rock' && textDiv.textContent === 'Rock') {
             return textDiv.textContent += ": It's a DRAW!";
-        } else if (textDiv.textContent === 'Scissors' && e.target.id === 'rock') {
+        } else if (e.target.id === 'rock' && textDiv.textContent === 'Scissors') {
             //add 1 to userWins
-            userWins += 1
-            yourScore.value = userWins;
+            userWinsTheGame()
+            console.log(yourScore.value);
+            //debugger;
             //update your-score <p> to = userWins variable
             return textDiv.textContent += ": YOU WIN!";
-        } else if (textDiv.textContent === 'Paper' && e.target.id === 'rock') {
+        } else if (e.target.id === 'rock' && textDiv.textContent === 'Paper') {
+            computerWinsTheGame();
             return textDiv.textContent += ": You lose!";
         }
 
         if(e.target.id === 'scissors' && textDiv.textContent === 'Rock') {
+            computerWinsTheGame();
             return textDiv.textContent += " : You lose!";
         } else if (e.target.id === 'scissors' && textDiv.textContent === 'Scissors') {
             return textDiv.textContent += ": It's a DRAW!";
-        } else if (e.target.id === 'scissors' && textDiv.textContent === 'Paper')
+        } else if (e.target.id === 'scissors' && textDiv.textContent === 'Paper') {
             return textDiv.textContent += ": YOU WIN!";
+        }
 
         if(e.target.id === 'paper' && textDiv.textContent === 'Rock') {
+            userWinsTheGame()
             return textDiv.textContent += ": YOU WIN!";
         } else if (e.target.id === 'paper' && textDiv.textContent === 'Paper') {
             return textDiv.textContent += ": It's a DRAW!";
         } else if (e.target.id === 'paper' && textDiv.textContent === 'Scissors') {
+            computerWinsTheGame();
             return textDiv.textContent += " : You lose!";
         }
     })
