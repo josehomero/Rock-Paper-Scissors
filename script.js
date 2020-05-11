cac72a9f03bdd819124697454ee92a04fc9d6af7
josehomero <jose.homero.rios@outlook.com>
2020-05-10 17:20:52 -0700

    working on updating user score

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const opponentResult = ['Rock', 'Paper', 'Scissors'];
+let userWins = 0;
+let computerWins = 0;
 
 const textDiv = document.getElementById('text-div');
+let yourScore = document.getElementsByTagName('your-score');
+let computerscore = document.getElementById('computer-score');
 const pictureButtons = document.querySelectorAll('.picture-buttons');
 pictureButtons.forEach(button => {
     button.addEventListener('click', e => {
         textDiv.textContent = opponentResult[Math.floor(Math.random() * 3)];
 
+
+
         if(e.target.id === 'rock' && textDiv.textContent === 'Rock') {
             return textDiv.textContent += ": It's a DRAW!";
         } else if (textDiv.textContent === 'Scissors' && e.target.id === 'rock') {
+            //add 1 to userWins
+            userWins += 1
+            yourScore.value = userWins;
+            //update your-score <p> to = userWins variable
             return textDiv.textContent += ": YOU WIN!";
         } else if (textDiv.textContent === 'Paper' && e.target.id === 'rock') {
             return textDiv.textContent += ": You lose!";
